21c1f596dbf52a74dafd5b1f99aaf2d50e152fd7
WesleyBalsimo <wbalsimo@icloud.com>
2026-05-07 11:55:46 -0500

    feat: added grid gen function

---
+++ Python Scripts/gridGen.py
@@ -0,0 +1,12 @@
+import machine
+from machine import Pin
+import functions
+import calibration
+
+def main():
+    calibration.sizeX
+    calibration.sizeY
+
+
+if __name__ == "__main__":
+    main()
