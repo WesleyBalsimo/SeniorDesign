73237c929ed4410b5b1d6c4ccfbd4155dd58a853
WesleyBalsimo <wbalsimo@icloud.com>
2026-05-08 08:54:12 -0500

    fix: make the grid gen fscript smarter

--- Python Scripts/gridGen.py
+++ Python Scripts/gridGen.py
@@ -15,10 +15,21 @@ def main():
     
     x = 0
     y = 0
+
+    tempSize0 = 0
+    tempSize = 0
+    
+    #Get the max size of the grid by checking each row/column to the biggest found so far
     for row in matrix:
-        x += 1
-        for string in row:
-            y += 1
+        tempSize0 += 1
+        if tempSize0 > x:
+            x = tempSize0
+        for cell in row:
+            tempSize += 1
+            if tempSize > y:
+                y = tempSize
+        tempSize = 0
+    tempSize0 = 0
 
     print('x: ' + str(x))
     print('y: ' + str(y))
