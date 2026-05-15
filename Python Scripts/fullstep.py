6a9c516bcdac8fdfff9a65b79914d97636cab872
Wesley <balsimowesleyg@johndeere.com>
2026-05-15 08:11:34 -0500

    add: functionality for demo in fullstep.py

--- Python Scripts/fullstep.py
+++ Python Scripts/fullstep.py
@@ -1,16 +1,39 @@
 # This is mostly a test file to debug and showcase movement
 
-
 from machine import Pin
-from time import sleep_ms
+from time import sleep
 import functions
   
 def main():
     size = 0
-    while(1):
+    while(size < 100):
         functions.fullstep_forward(functions.motor1)
         size = size + 1
     functions.mSleep(functions.motor1)
+    size = 0
+    sleep(1)
+
+    while(size < 100):
+        functions.halfstep_forward(functions.motor1)
+        size = size + 1
+    functions.mSleep(functions.motor1)
+    size = 0
+    sleep(1)
+
+    while(size < 100):
+        functions.fullstep_forward(functions.motor1)
+        size = size + 1
+    functions.mSleep(functions.motor1)
+    size = 0
+    sleep(1)
+
+    while(size < 100):
+        functions.halfstep_backward(functions.motor1)
+        size = size + 1
+    functions.mSleep(functions.motor1)
+    size = 0
+    sleep(1)
+
     print(size)
 
 if __name__ == "__main__":
